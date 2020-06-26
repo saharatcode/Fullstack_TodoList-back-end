075a8d9a4e0ab7b699c30c773e2c9eff1144bdc7
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-06-26 11:49:00 +0700

    init project

---
+++ src/components/pages/Register.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Form, Input, Button, Row, Col, Divider } from 'antd';
+import Title from 'antd/lib/typography/Title';
+
+const layout = {
+    labelCol: { xs: 24, sm: 7, md: 6, lg: 6, xl: 5, xxl: 4 },
+    wrapperCol: { xs: 24, sm: 17, md: 18, lg: 18, xl: 19, xxl: 20 },
+};
+export default function Register() {
+
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <Row justify="center" >
+            <Col xs={23} sm={23} md={23} lg={14} xl={14} xxl={12}>
+                <div
+                    className="Form"
+                >
+                    <Row justify="center">
+                        <Title level={2} className="Title">
+                            Register
+                        </Title>
+                    </Row>
+                    <Divider className="Divider" />
+                    <Form
+                        {...layout}
+                        onFinish={onFinish}
+                        style={{ width: "100%" }}
+                    >
+                        <Form.Item
+                            name="email"
+                            label="E-mail"
+                            rules={[
+                                {
+                                    type: 'email',
+                                    message: 'The input is not valid E-mail!',
+                                },
+                                {
+                                    required: true,
+                                    message: 'Please input your E-mail!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            label="Password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="confirm"
+                            label="Confirm Password"
+                            dependencies={['password']}
+                            // hasFeedback
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please confirm your password!',
+                                },
+                                ({ getFieldValue }) => ({
+                                    validator(rule, value) {
+                                        if (!value || getFieldValue('password') === value) {
+                                            return Promise.resolve();
+                                        }
+                                        return Promise.reject('The two passwords that you entered do not match!');
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="nickname"
+                            label={
+                                <span>
+                                    Nickname&nbsp;
+                        </span>
+                            }
+                            rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                            <Button className="Button" type="primary" htmlType="submit">
+                                Register
+                            </Button>
+                    </Form>
+                </div>
+            </Col>
+        </Row>
+    );
+}
